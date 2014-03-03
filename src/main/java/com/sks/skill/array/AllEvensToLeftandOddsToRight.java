2b7b7ace0305a7c1730afbece97cf6c31ecef195
Sabith K S <sks@apple.com>
2014-03-02 23:19:54 -0800

    AllEvensToLeftandOddsToRight

---
+++ src/main/java/com/sks/skill/array/AllEvensToLeftandOddsToRight.java
@@ -0,0 +1,37 @@
+package com.sks.skill.array;
+
+import java.util.Arrays;
+
+/**
+ * @author Sabith_ks
+ *
+ * Inspired by 
+ */
+public class AllEvensToLeftandOddsToRight {
+	
+	public static void main(String[] args){
+		new AllEvensToLeftandOddsToRight().sort(new Integer[]{1, 12,3, 34,54,6,3,12,23});
+	}
+
+	private Integer[] sort(Integer[] integers) {
+		int i =0,
+			j = integers.length-1;
+		while(i<j){
+			if(integers[i]%2==0){
+				//this is even, so move to next
+				i++;
+			}
+			if(integers[j]%2!=0){
+				j--;
+			}
+			//here we are , we have the odd at left, even at right. swap
+			if(i<j){
+				
+				//swap after making sure that we have not crossed the limits
+				integers[i] = integers[j]+integers[i] - (integers[j]=integers[i]);
+			}
+		}
+		System.out.println(Arrays.asList(integers));
+		return integers;
+	}
+}
